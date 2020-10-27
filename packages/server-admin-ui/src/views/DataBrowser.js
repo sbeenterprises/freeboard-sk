sbeenterprises/freeboard-sk

commit 3413b685f0f3008b394bbe7acef8b1aa675fe0e8

chore: optimize bundle size (#1169)
* chore: replace lodash use with individual imports

* chore: ignore moment locales to save in bundle size

packages/server-admin-ui/src/views/DataBrowser.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { keys, get, values } from 'lodash'
+import get from 'lodash.get'
 import JSONTree from 'react-json-tree'
 import {
   Badge,
@@ -35,9 +35,9 @@ function fetchRoot () {
     .then(response => response.json())
     .then(data => {
       if ( data.sources ) {
-        values(data.sources).forEach(source => {
+        Object.values(data.sources).forEach(source => {
           if ( source.type === "NMEA2000" ) {
-            keys(source).forEach(key => {
+            Object.keys(source).forEach(key => {
               let device = source[key]
               if ( device.n2k && device.n2k.productName ) {
                 source[`${device.n2k.manufacturerName || ''} ${device.n2k.productName} (${key})`] = device
@@ -97,7 +97,7 @@ class DataBrowser extends Component {
           let sentence = update.source && update.source.sentence && `(${update.source.sentence})`
           update.values.forEach(vp => {
             if ( vp.path === '' ) {
-              keys(vp.value).forEach(k => {
+              Object.keys(vp.value).forEach(k => {
                 context[k] = {
                   path: k,
                   value: vp.value[k],
@@ -239,7 +239,7 @@ class DataBrowser extends Component {
             onChange={this.handleContextChange}
           >
             <option value="none">Select a context</option>
-            {keys(this.state.data).sort().map(key => {
+            {Object.keys(this.state.data).sort().map(key => {
               return (
                   <option key={key} value={key}>{key}</option>
               )
@@ -312,7 +312,7 @@ class DataBrowser extends Component {
               </thead >
               <tbody>
 
-          { keys(this.state.data[this.state.context])
+          { Object.keys(this.state.data[this.state.context])
               .filter( key => {
                 return !this.state.search ||
                   this.state.search.length === 0 ||
@@ -327,7 +327,7 @@ class DataBrowser extends Component {
                 const formatted = JSON.stringify(
                   data.value,
                   null,
-                  typeof data.value === 'object' && keys(data.value).length > 1 ? 2 : 0)
+                  typeof data.value === 'object' && Object.keys(data.value).length > 1 ? 2 : 0)
                 const meta = this.state.data[this.state.context][data.path + '.meta']
                 const units = meta && meta.value.units ? meta.value.units : ''
                 const path = key.substring(0, key.lastIndexOf('.'))
@@ -358,7 +358,7 @@ class DataBrowser extends Component {
               </tr>
             </thead>
             <tbody>
-            {keys(this.state.data[this.state.context]).filter(key => { return key.endsWith('.meta') && ( !this.state.search || this.state.search.length === 0 || key.indexOf(this.state.search) !== -1) }).sort().map(key => {
+            {Object.keys(this.state.data[this.state.context]).filter(key => { return key.endsWith('.meta') && ( !this.state.search || this.state.search.length === 0 || key.indexOf(this.state.search) !== -1) }).sort().map(key => {
           const data = this.state.data[this.state.context][key]
           const formatted = JSON.stringify(data.value, null, 2)
           const path = data.path.substring(0, key.lastIndexOf('.'))

packages/server-admin-ui/src/views/Playground.js
@@ -1,6 +1,5 @@
 import React, { Component, useState } from 'react'
 import { connect } from 'react-redux'
-import { keys, get } from 'lodash'
 import {
   Badge,
   Button,
@@ -148,7 +147,7 @@ class Playground extends Component {
                 if ( update.values ) {
                   update.values.forEach(vp => {
                     if ( vp.path === '' ) {
-                      keys(vp.value).forEach(k => {
+                      Object.keys(vp.value).forEach(k => {
                         values.push({
                           path: k,
                           value: vp.value[k],
@@ -316,7 +315,7 @@ class Playground extends Component {
               <tbody>
 
           {this.state.data.map(data => {
-          const formatted = JSON.stringify(data.value, null, typeof data.value === 'object' && keys(data.value).length > 1 ? 2 : 0)
+          const formatted = JSON.stringify(data.value, null, typeof data.value === 'object' && Object.keys(data.value).length > 1 ? 2 : 0)
             const path = data.path
             const key = `${data.path}${data.context}`
 

packages/server-admin-ui/src/views/ServerConfig/ProvidersConfiguration.js
@@ -26,7 +26,7 @@ import {
 
 import BasicProvider from './BasicProvider'
 import SourcePriorities from './SourcePriorities'
-import { set } from 'lodash'
+import set from 'lodash.set'
 
 function fetchProviders () {
   fetch(`${window.serverRoutesPrefix}/providers`, {

packages/server-admin-ui/src/views/ServerConfig/SourcePriorities.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {Alert, Badge, Button, Card, CardHeader, CardBody, CardFooter, Collapse, Input, Table } from 'reactstrap'
 import Creatable from 'react-select/creatable'
-import { remove } from 'lodash'
+import remove from 'lodash.remove'
 
 export const SOURCEPRIOS_PRIO_CHANGED = 'SOURCEPRIOS_PPRIO_CHANGED'
 export const SOURCEPRIOS_PRIO_DELETED = 'SOURCEPRIOS_PRIO_DELETED'

packages/server-admin-ui/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
@@ -59,6 +60,10 @@ module.exports = {
       ]
     }, {
       copyUnmodified: false
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'disabled',

commit 3f77c2fc2393667b7d06616669de99ec3a05c3ed

fix: Failure in DataBrowser when swarching broken by #1169

packages/server-admin-ui/src/views/DataBrowser.js
@@ -327,7 +327,7 @@ class DataBrowser extends Component {
                 const formatted = JSON.stringify(
                   data.value,
                   null,
-                  typeof data.value === 'object' && Object.keys(data.value).length > 1 ? 2 : 0)
+                  typeof data.value === 'object' && Object.keys(data.value || {}).length > 1 ? 2 : 0)
                 const meta = this.state.data[this.state.context][data.path + '.meta']
                 const units = meta && meta.value.units ? meta.value.units : ''
                 const path = key.substring(0, key.lastIndexOf('.'))